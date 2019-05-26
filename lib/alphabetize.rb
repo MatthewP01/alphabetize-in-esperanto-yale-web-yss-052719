9dfebdc9effe0d9dd2af4e2013e4f1a793e4fbfa
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-26 23:09:12 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,6 +1,5 @@
 def alphabetize(arr)
-  def alphabetize(arr)
-  # code here	  arr.sort_by! do |a|
+  arr.sort_by! do |a|
     positions = []
     a.each_char do |char|
       positions << ESPERANTO_ALPHABET.index(char)
@@ -8,4 +7,4 @@ def alphabetize(arr)
     positions
   end
   arr
-end 	end 
+end 	end
