e0004a49609d57760e93b8fe17be30733b5685c9
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-26 23:13:26 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -13,7 +13,7 @@ def alphabetize(arr)
   # esperanto_alphabet = ["a", "b", "c", "ĉ", "d", "e", "f", "g", "ĝ", "h", "ĥ", "i", "j", "ĵ", "k", "l", "m", "n", "o", "p", "r", "s", "ŝ", "t", "u", "ŭ", "v", "z"]
   esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   arr.sort_by do |word|
-    word.collect do |letter|
+    word.split('').collect do |letter|
       esperanto_alphabet.index(letter)
     end
   end
