d387adc556050afaee526fcdfd033640c715068c
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-26 23:12:33 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,10 +1,19 @@
+# def alphabetize(arr)
+#   esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+#   arr.sort_by! do |a|
+#     alphabetized = []
+#     a.each_char do |char|
+#       positions << esperanto.index(char)
+#     end
+#     return alphabetized
+#   end
+# end
+
 def alphabetize(arr)
-  esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  arr.sort_by! do |a|
-    alphabetized = []
-    a.each_char do |char|
-      positions << esperanto.index(char)
+  esperanto_alphabet = ["a", "b", "c", "ĉ", "d", "e", "f", "g", "ĝ", "h", "ĥ", "i", "j", "ĵ", "k", "l", "m", "n", "o", "p", "r", "s", "ŝ", "t", "u", "ŭ", "v", "z"]
+  arr.sort_by do |word|
+    word.split('').collect do |letter|
+      esperanto_alphabet.index(letter)
     end
-    return alphabetized
   end
 end
